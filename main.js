1377f2347fa2cec1bec1ff123c0ddf8d1b672bf0
DevSilva <jerson.silva99@hotmail.com>
2022-09-28 14:10:43 -0500

    fix: Fixing draw html, css and js

--- main.js
+++ main.js
@@ -4,63 +4,70 @@ import Split from 'split-grid'
 const $ = selector => document.querySelector(selector)
 
 Split({
-    columnGutters: [{
-        track: 1,
-        element: $('.gutter-col-1')
-    }],
-    rowGutters: [{
-        track: 1,
-        element: $('.gutter-row-1')
-    }]
+  columnGutters: [{
+      track: 1,
+      element: $('.gutter-col-1')
+  }],
+  rowGutters: [{
+      track: 1,
+      element: $('.gutter-row-1')
+  }]
 })
 
-// Selectores de id de los textarea
-
-
-const $js = $('#js')
-const $css = $('#css')
 const $html = $('#html')
+const $css = $('#css')
+const $js = $('#js')
 
-// Eventos para que se actualice el doc del html, css y js
+$html.addEventListener('input', update)
+$css.addEventListener('input', update)
+$js.addEventListener('input', update)
 
-$js.addEventListener('input', Update)
-$css.addEventListener('input', Update)
-$html.addEventListener('input', Update)
+function init () {
+  const { pathname } = window.location
 
-function Update () {
-    const html = $html.value
-    const css = $css.value
-    const js = $js.value
+  const [decodeHtml, decodeCss, decodeJs] = pathname.slice(1).split('%7C')
 
-    // Generar URL al escribir código
-    const hashedCode = ` ${ window.btoa(html) } | ${ window.btoa(css) } | ${ window.btoa(js) } `
-    window.history.replaceState(null, null, `/${hashedCode}`)
+  const html = window.atob(decodeHtml)
+  const css = window.atob(decodeCss)
+  const js = window.atob(decodeJs)
 
-    
-    const htmlForPreview = CreateHTML({ html, css, js })
-    $('iframe').setAttribute('srcdoc', htmlForPreview)
+  $html.value = html
+  $css.value = css
+  $js.value = js
+
+  const htmlForPreview = createHtml({ html, css, js })
+  $('iframe').setAttribute('srcdoc', htmlForPreview)
 }
 
-// Crea estructura de html cada vez que se cree un documento nuevo
+function update () {
+  const html = $html.value
+  const css = $css.value
+  const js = $js.value
 
-const CreateHTML = ({ html, css, js }) => {
-    return `
+  const hashedCode = `${window.btoa(html)}|${window.btoa(css)}|${window.btoa(js)}`
+  window.history.replaceState(null, null, `/${hashedCode}`)
+
+  const htmlForPreview = createHtml({ html, css, js })
+  $('iframe').setAttribute('srcdoc', htmlForPreview)
+}
+
+const createHtml = ({ html, css, js }) => {
+  return `
     <!DOCTYPE html>
     <html lang='es'>
-    <head>
-        <meta charset='UTF-8'>
-        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+      <head>
         <style>
-            ${ css }
+          ${css}
         </style>
-    </head>
-    <body>
-    <script>
-        ${ js }
-    </script>
-    ${ html }
-</body>
-</html>
-    `
+      </head>
+      <body>
+        ${html}
+      </body>
+      <script>
+        ${js}
+      </script>
+    </html>
+  `  
 }
+
+init()
