566558ed59b497d21f93bb6b437632a96297a8dc
DevSilva <jerson.silva99@hotmail.com>
2022-10-01 14:48:48 -0500

    feat: hotkeys in the editor

---
+++ src/hotkeys.js
@@ -0,0 +1,17 @@
+import hotkeys from "hotkeys-js";
+
+hotkeys('ctrl+a,ctrl+b,b,r,f', function (event, handler) {
+    console.log(event)
+
+    switch (handler.key) {
+        case 'ctrl+a': window.alert('Precionaste la combinación de ctrl + a')
+        break
+        case 'ctrl+b': window.alert('Precionaste la combinación de ctrl + b')
+          break
+        case 'r': window.alert('Precionaste la tecla r')
+          break
+        case 'f': window.alert('Precionaste la tecla f')
+          break
+        default: window.alert(event)
+    }
+})
