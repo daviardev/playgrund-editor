7637a9fbcd4d8a6e91e9c757376333c65c098a4e
DevSilva <jerson.silva99@hotmail.com>
2022-10-01 14:48:01 -0500

    feat: Add configurations of the editor monaco

--- src/editor.js
+++ src/editor.js
@@ -1,6 +1,10 @@
 import * as monaco from 'monaco-editor'
 import { emmetHTML } from 'emmet-monaco-es'
 
+import HtmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import JsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import CssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+
 const COMMON_EDITOR_OPTIONS = {
   automaticLayout: true,
   fontSize: 18,
@@ -13,13 +17,21 @@ const COMMON_EDITOR_OPTIONS = {
   padding: {
     top: 16
   },
-  lineNumbers: false,
+  lineNumbers: 'off',
   minimap: {
     enabled: false
   }
 }
 
 emmetHTML(monaco)
+
+window.MonacoEnvironment = {
+  getWorker (_, label) {
+    if (label === 'html') return new HtmlWorker()
+    if (label === 'javascript') return new JsWorker()
+    if (label === 'css') return new CssWorker()
+  }
+}
 
 export const createEditor = ({ domElement, language, value }) => {
   return monaco.editor.create(domElement, {
