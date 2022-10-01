114a73fdb7e03822469826d2381304d10b60e5a6
DevSilva <jerson.silva99@hotmail.com>
2022-10-01 14:51:18 -0500

    feat: Using variables $ for the querySelector and selector all

---
+++ src/utils/dom.js
@@ -0,0 +1,7 @@
+export const $ = (selector, context = document) => {
+    context.querySelector(selector)
+}
+
+export const $$ = (selector, context = document) => {
+    context.querySelectorAll(selector)
+}
