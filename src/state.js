2c3b08a56c16dc854fc78506bc15fe02993a0b27
DevSilva <jerson.silva99@hotmail.com>
2022-10-02 15:21:07 -0500

    refactor: Add wordWrap in the state, for default is enable

--- src/state.js
+++ src/state.js
@@ -8,7 +8,8 @@ const appInitialState = getLocalStorage('appInitialState') || {
     fontSize: 18,
     lineNumbers: 'off',
     minimap: false,
-    theme: 'vs-dark'
+    theme: 'vs-dark',
+    wordWrap: 'on'
 }
 
 const useStore = create(set => ({
