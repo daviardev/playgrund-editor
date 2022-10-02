eacabff8f56c34636ceb1e508eedf99ae394080c
DevSilva <jerson.silva99@hotmail.com>
2022-10-02 16:46:33 -0500

    refactor: Add settings for that save options of the inputs and checkbox

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,28 @@
 import { $$ } from './utils/dom.js'
-import { setState } from './state.js'
+import { getState } from './state.js'
 
-$$('#settings [data-for]').forEach(el => {
+const $settings = $$('#settings [data-for]')
+
+const {
+  updateSettings,
+  ...settings
+} = getState()
+
+$settings.forEach(el => {
+    const settingKey = el.getAttribute('data-for')
+    const actualSettingValue = settings[settingKey]
+    const isSelect = el.nodeName === 'SELECT'
+    const isCheckbox = el.nodeName === 'INPUT' && el.type === 'checkbox'
+
+    if (isSelect) {
+      const optionsToSelect = el.querySelector(`option[value="${actualSettingValue}"`)
+      if (!optionsToSelect) return console.warn('[settings] Opción escogida no se encontró')
+      optionsToSelect.setAttribute('selected', '')
+    }
+
+    if (isCheckbox) el.checked = actualSettingValue
+    else el.value = actualSettingValue
+
     el.addEventListener('change', ({ target }) => {
         const { checked, value } = target
         const settingKey = target.getAttribute('data-for')
@@ -10,8 +31,9 @@ $$('#settings [data-for]').forEach(el => {
         let settingValue = typeof checked === 'boolean' ? checked : value
         if (isNumber) settingValue = +value
 
-        setState({
-          [settingKey]: settingValue
+        updateSettings({
+          key: settingKey,
+          value: settingValue
         })
     })
 })
