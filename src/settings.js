ac3e461d605e6ba0d56ae168d0cea5e4cbff9593
DevSilva <jerson.silva99@hotmail.com>
2022-10-02 20:41:38 -0500

    refactor: Using form for that the code is clean

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-import { $$ } from './utils/dom.js'
+import { $$, setFormControlValue } from './utils/dom.js'
 import { getState } from './state.js'
 
 const $settings = $$('#settings [data-for]')
@@ -9,31 +9,22 @@ const {
 } = getState()
 
 $settings.forEach(el => {
-    const settingKey = el.getAttribute('data-for')
-    const actualSettingValue = settings[settingKey]
-    const isSelect = el.nodeName === 'SELECT'
-    const isCheckbox = el.nodeName === 'INPUT' && el.type === 'checkbox'
+  const settingKey = el.getAttribute('data-for')
+  const actualSettingValue = settings[settingKey]
 
-    if (isSelect) {
-      const optionsToSelect = el.querySelector(`option[value="${actualSettingValue}"`)
-      if (!optionsToSelect) return console.warn('[settings] Opción escogida no se encontró')
-      optionsToSelect.setAttribute('selected', '')
-    }
+  setFormControlValue(el, actualSettingValue)
 
-    if (isCheckbox) el.checked = actualSettingValue
-    else el.value = actualSettingValue
+  el.addEventListener('change', ({ target }) => {
+    const { checked, value } = target
+    const settingKey = target.getAttribute('data-for')
+    const isNumber = target.getAttribute('type') === 'number'
 
-    el.addEventListener('change', ({ target }) => {
-        const { checked, value } = target
-        const settingKey = target.getAttribute('data-for')
-        const isNumber = target.getAttribute('type') === 'number'
+    let settingValue = typeof checked === 'boolean' ? checked : value
+    if (isNumber) settingValue = +value
 
-        let settingValue = typeof checked === 'boolean' ? checked : value
-        if (isNumber) settingValue = +value
-
-        updateSettings({
-          key: settingKey,
-          value: settingValue
-        })
+    updateSettings({
+      key: settingKey,
+      value: settingValue
     })
+  })
 })
--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,2 +1,18 @@
 export const $$ = selector => document.querySelectorAll(selector)
 export const $ = selector => document.querySelector(selector)
+export const isNodeSelect = el => el.nodeName === 'SELECT'
+export const isNodeCheckbox = el => el.nodeName === 'INPUT' && el.type === 'checkbox'
+
+const updateSelectValue = (el, value) => {
+  const optionsToSelect = el.querySelector(`option[value="${value}"`)
+  if (!optionsToSelect) return console.warn('Opción escogida no se encontró')
+  optionsToSelect.setAttribute('selected', '')
+}
+export const setFormControlValue = (el, value) => {
+  const isSelect = isNodeSelect(el)
+  const isCheckbox = isNodeCheckbox(el)
+
+  if (isSelect) updateSelectValue(el, value)
+  else if (isCheckbox) el.checked = value
+  else el.value = value
+}
