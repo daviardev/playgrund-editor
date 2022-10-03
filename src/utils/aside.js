0017d0915345830873de0cc172593dbfd762ee7e
DevSilva <jerson.silva99@hotmail.com>
2022-10-03 13:55:37 -0500

    refactor: Hidden and show side bar

--- src/utils/aside.js
+++ src/utils/aside.js
@@ -1,20 +1,30 @@
 import { $, $$ } from './dom.js'
 
 const $aside = $('aside')
-const $views = $$('.view')
 const $buttons = $$('button', $aside)
+
+const actions = {
+  'close-aside-bar': () => {
+    $('.aside-bar').setAttribute('hidden', '')
+  },
+  'show-skypack-bar': () => {
+    $('.aside-bar').removeAttribute('hidden')
+    $$('.bar-content').forEach(el => el.setAttribute('hidden', ''))
+    $('#skypack').removeAttribute('hidden')
+  },
+  'show-settings-bar': () => {
+    $('.aside-bar').removeAttribute('hidden')
+    $$('.bar-content').forEach(el => el.setAttribute('hidden', ''))
+    $('#settings').removeAttribute('hidden')
+  }
+}
 
 $buttons.forEach(button => {
   button.addEventListener('click', ({ currentTarget }) => {
     $('.is-active').classList.remove('is-active')
     currentTarget.classList.add('is-active')
 
-    const elementIdToShow = button.getAttribute('data-to')
-
-    $views.forEach(view => {
-      view.setAttribute('hidden', '')
-    })
-
-    $(`#${elementIdToShow}`).removeAttribute('hidden')
+    const action = button.getAttribute('data-action')
+    actions[action]()
   })
 })
