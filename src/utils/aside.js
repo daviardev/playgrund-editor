747abc859f550b9238650117705234a8d67cdbe7
DevSilva <jerson.silva99@hotmail.com>
2022-10-02 21:48:27 -0500

    refactor: Add border line when option of the file or settings is active

--- src/utils/aside.js
+++ src/utils/aside.js
@@ -5,8 +5,12 @@ const $views = $$('.view')
 const $buttons = $$('button', $aside)
 
 $buttons.forEach(button => {
-  button.addEventListener('click', () => {
+  button.addEventListener('click', ({ currentTarget }) => {
+    $('.is-active').classList.remove('is-active')
+    currentTarget.classList.add('is-active')
+
     const elementIdToShow = button.getAttribute('data-to')
+
     $views.forEach(view => {
       view.setAttribute('hidden', '')
     })
