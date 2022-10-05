7c38aae85ab0cc8cf663cdc6f680b23bc0d216f3
DevSilva <jerson.silva99@hotmail.com>
2022-10-05 11:06:40 -0500

    remove: Remove app shortUrl and remove pre-commit

--- api/shortUrl.py
+++
@@ -1,9 +0,0 @@
-import pyshorteners as shr
-
-short = shr.Shortener()
-
-url = input("Ingresa la URL: ") # Este campo estará el botón que copiará la URL.d
-
-urlShort = short.tinyurl.short(url)
-
-print(f"{urlShort}") # Este campo copiará y pegará en el portapapeles la URL acortada.
